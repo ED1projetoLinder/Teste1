6bf75458e1a939d4067419a8d88924e41d775169
VitoBragarUnivasf <joaovitor918@hotmail.com>
2017-10-04 20:15:55 -0300

    Update ProgramaAero.h

--- ProgramaAero.h
+++ ProgramaAero.h
@@ -1,3 +1,13 @@
-#include <Stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#define HORAMAX 24 
 
+typedef struct dados
+{
+	char empresa[60];
+	char cidade[50];
+	int numpass;
+	int horario;
+	int ativo;
+	
+}dados;
