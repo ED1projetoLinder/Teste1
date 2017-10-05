89d90d874759333ae875cab3d3c3b0e0c2a34d9c
VitoBragarUnivasf <joaovitor918@hotmail.com>
2017-10-04 21:03:48 -0300

    Update ProgramaAero.h

--- ProgramaAero.h
+++ ProgramaAero.h
@@ -5,9 +5,29 @@
 typedef struct dados
 {
 	char empresa[60];
+	char ident[5];
 	char cidade[50];
 	int numpass;
 	int horario;
 	int ativo;
-	
+	int pista;
 }dados;
+
+
+typedef struct nodo
+{
+	dados inf;
+	struct nodo *next; 
+}nodo;
+
+typedef nodo* pistas;
+typedef nodo* listavoos;
+// falta definir o de árvore;
+
+
+void cria_voo(listavoos*,dados);
+void remove_voo(listavoos*,dados);
+void gerenciar_pistas(pistas*);
+void listar_ult_voos(listavoos);
+void listar_prox_voos(listavoos);
+void listar_tds_voos(listavoos);
