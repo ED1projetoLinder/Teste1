91a23e456336f5c0aedaa00fddd8b744c11b3d4b
VitoBragarUnivasf <joaovitor918@hotmail.com>
2017-10-05 19:23:56 -0300

    Create aero.c

---
+++ aero.c
@@ -0,0 +1,63 @@
+void cria_voo(listavoos *l,dados inf)
+{
+	int horavoo=-1,cont;
+	nodo *novo;
+	listavoos aux=*l;
+	novo= malloc (sizeof (nodo));
+	
+	if(!novo)
+	{
+		printf("falta de memoria\n");
+		exit(1);
+	}
+	else
+	{
+		do
+		{
+			printf("\ninforme o horário em que o voo sera inserido(entre 0 e 23), no formato hh. ex: 16 para 16:00 ");
+			scanf("%d",&horavoo);
+			if(horavoo<0||horavoo>23)
+				printf("\nhora invalida, somente valido entre 0 e 23");
+		}while(horavoo<0||horavoo>23);
+	
+	while(aux->inf.horario!=horavoo)
+		aux=aux->next;
+		
+	for(cont=0;aux->inf.horario!=horavoo+1;cont++)
+		aux=aux->next;	
+		
+	if(cont>NUMPISTAS)
+	{
+		printf("\ntodas as pistas ja estao ocupadas");
+		system("PAUSE");
+		//menu();
+	}
+	else
+	{
+		novo->next=aux->next;
+		aux->next=novo;
+		do
+		{
+		
+		printf("\nO voo e de chegada ou saida?(0 pra ida,1 para chegada)       ");
+		scanf("%d",&novo->inf.modo);
+	 	}while(novo->inf.modo==0||novo->inf.modo==1);
+		 if(novo->inf.modo==0)
+	 		{
+			printf("\ndigite qual a cidade destino:    ");
+	 	}else
+	 	{
+	 		printf("\ndigite a cidade de qual o voo vem:    ");
+		}
+		
+		scanf("%s",&novo->inf.cidade);
+		printf("\ninforme a empresa que fornece o voo:      ");
+		novo->inf.horario=horavoo;
+		printf("\ninsira numero de identificacao do aviao utilizado:      ");
+		scanf("%s",&novo->inf.num_id);
+		novo->inf.pista=cont+1;
+	}
+	
+	
+	}
+}
