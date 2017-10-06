20db2e232ba1ddc676c10e1f5a73bf403db8de47
VitoBragarUnivasf <joaovitor918@hotmail.com>
2017-10-06 16:34:21 -0300

    Update aero.c

--- aero.c
+++ aero.c
@@ -1,19 +1,41 @@
 #include "aero.h"
 
 
+void insere_fim(lista_voos *l,dados valores)
+{
+	nodo *novo;
+	lista_voos aux;
+	novo = (nodo*) malloc(sizeof(nodo));
+	if(!novo)
+	{
+		printf("nao ha memoria no sistema");
+	}
+	novo->inf=valores;
+
+	for(;aux->next!=NULL;aux=aux->next);
+	novo->next=aux->next;
+	aux->next=novo;
+	free(aux);
+}
+
+
 void lerarquivo(lista_voos *l,FILE *arq)
 {
 	arq= fopen("listadevoos.dat","rb");
+	dados info_voo;
+	lista_voos aux=*l;
 	if(arq)
 	{
 		while(!feof(arq))
-		{
-			fread(&((*l)->inf),sizeof(dados),1,arq);
-			*l=(*l)->next;
+		{	
+			fread(&info_voo,sizeof(dados),1,arq);
+			insere_fim(&aux,info_voo);
+			aux=aux->next;
 		}
 	}
 	fclose(arq);
 }
+
 void salvarlista(lista_voos l,FILE *arq)
 {
 	
@@ -31,13 +53,31 @@ void salvarlista(lista_voos l,FILE *arq)
 
 }
 
+void salvarlista(lista_voos l,FILE *arq)
+{
+	
+	arq= fopen("listadevoos.dat","ab");
+	if(arq)
+	{
+		while(!l)
+		{
+			fwrite(&(l->inf),sizeof(dados),1,arq);
+			l=l->next;
+		}
+	}
+	
+	fclose(arq);
+
+}
+
 void cria_voo(lista_voos *l,FILE *arq)
 {
 	int horavoo=-1,cont;
 	nodo *novo;
 	lista_voos aux=*l;
-	novo= malloc (sizeof (nodo));
-	if(!novo)
+	novo=(nodo*) malloc (sizeof (nodo));
+	
+	if(!novo)//teste de memoria
 	{
 		printf("falta de memoria\n");
 		exit(1);
@@ -52,13 +92,10 @@ void cria_voo(lista_voos *l,FILE *arq)
 				printf("\nhora invalida, somente valido entre 0 e 23");
 		}while(horavoo<0||horavoo>23);
 	
-	while(aux->inf.horario!=horavoo)
-		aux=aux->next;
+	for(;aux->inf.horario<horavoo||aux!=NULL;aux=aux->next);//enquanto os voos forem menor que o horavoo, passa pro próximo, até encontrar a posição
+	for(cont=0;aux->inf.horario==horavoo;cont++,aux=aux->next);	//encontrando se tem voos iguais
 		
-	for(cont=0;aux->inf.horario!=horavoo+1;cont++)
-		aux=aux->next;	
-		
-	if(cont>NUMPISTAS)
+	if(cont>=NUMPISTAS)//se cont for igual ou maior que o numero de pistas, não há pistas disponíveis
 	{
 		printf("\ntodas as pistas ja estao ocupadas");
 		system("PAUSE");
@@ -84,6 +121,7 @@ void cria_voo(lista_voos *l,FILE *arq)
 		
 		scanf("%s",&novo->inf.cidade);
 		printf("\ninforme a empresa que fornece o voo:      ");
+		scanf("%s",&novo->inf.empresa);
 		novo->inf.horario=horavoo;
 		printf("\ninsira numero de identificacao do aviao utilizado:      ");
 		scanf("%s",&novo->inf.num_id);
@@ -95,5 +133,3 @@ void cria_voo(lista_voos *l,FILE *arq)
 	
 	}
 }
-
-
