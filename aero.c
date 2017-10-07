199f6487ac1751f7914eb8ef4e58755ad45305fd
VitoBragarUnivasf <joaovitor918@hotmail.com>
2017-10-06 22:32:03 -0300

    Update aero.c

--- aero.c
+++ aero.c
@@ -1,6 +1,31 @@
 #include "aero.h"
 
 
+void maketree(ARV_BIN_ENC *t, dados x)
+{
+	*t = (ARV_BIN_ENC) malloc (sizeof (arv_nodo));
+	if (!(*t))
+	{
+		printf("Erro! Nao existe memoria disponivel!");
+		exit (1);
+	}
+	(*t)->info = x;
+	(*t)->left = NULL;
+	(*t)->right = NULL;
+	(*t)->father = NULL;
+}
+
+void ins_ele (ARV_BIN_ENC *arv, dados v)
+{
+	if (!(*arv))
+		maketree(arv, v);
+	else
+		if (v.horario<(*arv)->info.horario)
+			ins_ele(&((*arv)->left), v);
+		else
+			ins_ele(&((*arv)->right), v);
+}	
+
 void insere_fim(lista_voos *l,dados valores)
 {
 	nodo *novo;
@@ -18,7 +43,6 @@ void insere_fim(lista_voos *l,dados valores)
 	free(aux);
 }
 
-
 void lerarquivo(lista_voos *l,FILE *arq)
 {
 	arq= fopen("listadevoos.dat","rb");
@@ -28,31 +52,13 @@ void lerarquivo(lista_voos *l,FILE *arq)
 	{
 		while(!feof(arq))
 		{	
-			fread(&info_voo,sizeof(dados),1,arq);
-			insere_fim(&aux,info_voo);
-			aux=aux->next;
+				fread(&info_voo,sizeof(dados),1,arq);
+				insere_fim(&aux,info_voo);
+				aux=aux->next;
 		}
 	}
 	fclose(arq);
 }
-
-void salvarlista(lista_voos l,FILE *arq)
-{
-	
-	arq= fopen("listadevoos.dat","ab");
-	if(arq)
-	{
-		while(!l)
-		{
-			fwrite(&(l->inf),sizeof(dados),1,arq);	
-			l=l->next;
-		}
-	}
-	
-	fclose(arq);
-
-}
-
 void salvarlista(lista_voos l,FILE *arq)
 {
 	
@@ -70,7 +76,7 @@ void salvarlista(lista_voos l,FILE *arq)
 
 }
 
-void cria_voo(lista_voos *l,FILE *arq)
+void cria_voo(lista_voos *l,FILE *arq,ARV_BIN_ENC *t)
 {
 	int horavoo=-1,cont;
 	nodo *novo;
@@ -107,29 +113,83 @@ void cria_voo(lista_voos *l,FILE *arq)
 		aux->next=novo;
 		do
 		{
-		
-		printf("\nO voo e de chegada ou saida?(0 pra ida,1 para chegada)       ");
-		scanf("%d",&novo->inf.modo);
+			printf("\nO voo e de chegada ou saida?(0 pra ida,1 para chegada)       ");
+			scanf("%d",&novo->inf.modo);
 	 	}while(novo->inf.modo==0||novo->inf.modo==1);
-		 if(novo->inf.modo==0)
-	 		{
+		if(novo->inf.modo==0)
+	 	{
 			printf("\ndigite qual a cidade destino:    ");
 	 	}else
 	 	{
 	 		printf("\ndigite a cidade de qual o voo vem:    ");
 		}
 		
-		scanf("%s",&novo->inf.cidade);
+		scanf("%s",novo->inf.cidade);
 		printf("\ninforme a empresa que fornece o voo:      ");
-		scanf("%s",&novo->inf.empresa);
+		scanf("%s",novo->inf.empresa);
 		novo->inf.horario=horavoo;
 		printf("\ninsira numero de identificacao do aviao utilizado:      ");
-		scanf("%s",&novo->inf.num_id);
+		scanf("%d",&novo->inf.num_id);
 		novo->inf.pista=cont+1;
 		
 		salvarlista(*l,arq);
+		if(novo->inf.horario);
 		free(aux);
 	}	
 	
 	}
+}
+
+
+
+dados busca_voo(ARV_BIN_ENC t)
+{
+	int horario,contador;
+	char local[50];
+	short int ida_volta;
+	
+	
+	do
+	{
+		printf("\nInforme se o voo esta chegando ou saindo(0 para saida, 1 para chegada)  ");
+		scanf("%d",&ida_volta);
+	}while(ida_volta!=0||ida_volta!=1);
+	
+	printf("\ninforme a cidade:   ");
+	scanf("%s",local);	
+	
+	do
+	{
+		printf("\nselecione o horario do voo procurado:    ");
+		scanf("%d",&horario);
+	}while(horario<0||horario>23);
+	
+	while(!t)
+	{
+		if(horario<t->info.horario)
+		{
+			t=t->left;
+		}else
+		{
+			if(horario>t->info.horario)
+			{
+				t=t->right;
+			}else
+			{
+				if(horario==t->info.horario)
+				{
+					if(t->info.cidade==local)
+					 	return t->info;
+					else
+						t=t->right;
+				}else
+				{
+					printf("nao ha voos neste horario");
+					exit(1);
+				}
+			}
+		}
+		
+	}	
+	
 }
